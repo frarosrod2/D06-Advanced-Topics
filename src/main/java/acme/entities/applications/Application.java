b0832c45aab30bbb4a458da5bbe9295fbdfaecf0
Paco <frarosrod2@alum.us.es>
2020-08-21 12:30:58 +0200

    Application index

--- src/main/java/acme/entities/applications/Application.java
+++ src/main/java/acme/entities/applications/Application.java
@@ -4,7 +4,9 @@ package acme.entities.applications;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -23,6 +25,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "ticker asc, creationMoment desc")
+})
 public class Application extends DomainEntity {
 
 	private static final long	serialVersionUID	= 1L;
