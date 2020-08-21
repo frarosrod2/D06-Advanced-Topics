d44629e19f1da8456e3cdaf15b1affa8a9cf28f6
Paco <frarosrod2@alum.us.es>
2020-08-21 16:31:21 +0200

    Random banner fixed

--- src/main/java/acme/components/BannerRepository.java
+++ src/main/java/acme/components/BannerRepository.java
@@ -4,11 +4,11 @@ package acme.components;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
-import org.springframework.boot.Banner;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import acme.entities.banners.Banner;
 import acme.framework.repositories.AbstractRepository;
 
 @Repository
